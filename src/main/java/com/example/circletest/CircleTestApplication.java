a770970131ae01101cc1d01dcaaf6538edabd215
Aaron Ilovici <aaron.ilovici@connexta.com>
2019-07-09 08:43:22 -0400

    Initial Commit for a Spring Boot app

---
+++ src/main/java/com/example/circletest/CircleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.circletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleTestApplication.class, args);
+	}
+
+}
