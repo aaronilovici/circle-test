68854dcdc0e2c840856f938bd7d7299ff88da439
Aaron Ilovici <aaron.ilovici@connexta.com>
2019-07-11 11:54:47 -0400

    Playing with sonar rules

--- src/main/java/com/example/circletest/CircleTestApplication.java
+++ src/main/java/com/example/circletest/CircleTestApplication.java
@@ -9,20 +9,20 @@ public class CircleTestApplication {
 	public static void main(String[] args) {
 		try {
 			something(1);
-		} catch (Exception e) {
+		} catch (NullPointerException e) {
 		}
 
 		SpringApplication.run(CircleTestApplication.class, args);
 	}
 
 	// Uncomment this test to fail the Circle 'sonar' build step
-	public static void something(int param) throws Exception {
+	public static void something(int param) throws NullPointerException {
 		switch (param) {
 			case 0:
 				System.out.println("BLAH");
 				break;
 			default:
-				throw new Exception();
+				throw new NullPointerException();
 			case 1:
 				System.out.println("BLAH TWICE");
 				break;
