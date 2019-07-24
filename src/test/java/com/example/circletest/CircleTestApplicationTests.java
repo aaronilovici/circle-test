fc825b2972d1bd83098900fee21524ec92ac4bde
Aaron Ilovici <aaron.ilovici@connexta.com>
2019-07-24 15:56:32 -0400

    Testing the metrics of test time

--- src/test/java/com/example/circletest/CircleTestApplicationTests.java
+++ src/test/java/com/example/circletest/CircleTestApplicationTests.java
@@ -5,13 +5,21 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static java.lang.Thread.sleep;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CircleTestApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
+
+	@Test
+    public void takeALongTimeTest() throws InterruptedException {
+	    sleep(5000);
+    }
 
 	// Uncomment this test to fail the Circle 'test' build step
 	/*
