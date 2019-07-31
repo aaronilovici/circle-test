66669c60384ac6cd15472bf9cb54e9b1d3a4e3aa
Aaron Ilovici <aaron.ilovici@connexta.com>
2019-07-31 11:24:52 -0400

    more bad code

---
+++ src/main/java/com/example/circletest/Util.java
@@ -0,0 +1,48 @@
+package com.example.circletest;
+
+import java.io.File;
+import java.net.*;
+import java.util.Enumeration;
+
+public class Util {
+
+
+    public void fileReader() {
+        File file = null;
+
+        if (file != null) {
+            try {
+                getFirstNonLoopbackAddress(true, false);
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static InetAddress getFirstNonLoopbackAddress(boolean preferIpv4, boolean preferIPv6)
+            throws SocketException {
+        Enumeration en = NetworkInterface.getNetworkInterfaces();
+        while (en.hasMoreElements()) {
+            NetworkInterface i = (NetworkInterface) en.nextElement();
+            Enumeration en2 = i.getInetAddresses();
+            while (en2.hasMoreElements()) {
+                InetAddress addr = (InetAddress) en2.nextElement();
+                if (!addr.isLoopbackAddress()) {
+                    if (addr instanceof Inet4Address) {
+                        if (preferIPv6) {
+                            continue;
+                        }
+                        return addr;
+                    }
+                    if (addr instanceof Inet6Address) {
+                        if (preferIpv4) {
+                            continue;
+                        }
+                        return addr;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
