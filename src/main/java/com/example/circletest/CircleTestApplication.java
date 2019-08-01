52f6948cda0e7360a519ead09a131901371b6b61
Aaron Ilovici <aaron.ilovici@connexta.com>
2019-08-01 11:44:26 -0400

    Still updating branch

--- src/main/java/com/example/circletest/CircleTestApplication.java
+++ src/main/java/com/example/circletest/CircleTestApplication.java
@@ -3,82 +3,10 @@ package com.example.circletest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.net.SocketException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-
 @SpringBootApplication
 public class CircleTestApplication {
 
-	private List<Integer> list = Arrays.asList(1, 2, 3, 4);
-	private List<Integer> list2 = Arrays.asList(1, 2, 3, 4);
-	private List<Integer> list5 = Arrays.asList(1, 2, 3, 4);
-
-	public static void main(String[] args) throws SocketException {
-		try {
-			something(1);
-		} catch (NullPointerException e) {
-		}
-
-		Util.getFirstNonLoopbackAddress(true, true);
+	public static void main(String[] args){
 		SpringApplication.run(CircleTestApplication.class, args);
-	}
-
-	// Uncomment this test to fail the Circle 'sonar' build step
-	public static void something(int param) throws NullPointerException {
-		switch (param) {
-			case 0:
-				System.out.println("BLAH");
-				break;
-			default:
-				throw new NullPointerException();
-			case 1:
-				System.out.println("BLAH TWICE");
-				break;
-		}
-	}
-
-	public static void somethingelse(int param) throws NullPointerException {
-		switch (param) {
-			case 0:
-				System.out.println("BLAH");
-				break;
-			default:
-				throw new NullPointerException();
-			case 1:
-				System.out.println("BLAH TWICE");
-				break;
-		}
-	}
-
-	public static void somethingalso(int param) throws NullPointerException {
-		switch (param) {
-			case 0:
-				System.out.println("BLAH");
-				break;
-			default:
-				throw new NullPointerException();
-			case 1:
-				System.out.println("BLAH TWICE");
-				break;
-		}
-	}
-
-	public static int lastbutnotleast(int param) throws NullPointerException {
-		if (param == 1) {
-			param =+ 1;
-		}
- 		return param;
-	}
-
-	public String badCode () {
-		list.add(1);
-		return "THIS IS BAD CODE";
-	}
-
-	public String alsoBadCode () {
-		list.add(1);
-		return "THIS IS BAD CODE";
 	}
 }
--- src/test/java/com/example/circletest/CircleTestApplicationTest.java
+++ src/test/java/com/example/circletest/CircleTestApplicationTest.java
@@ -1,2 +1,16 @@
+package com.example.circletest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CircleTestApplicationTest {
+
+  @Test
+  public void contextLoads() {
+
+  }
 }
