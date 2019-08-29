36d9405414265c7868095ee350139d547b3824f0
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-08-29 08:36:29 -0700

    Ignore InterruptedException

--- src/main/java/com/example/circletest/CircleTestApplication.java
+++ src/main/java/com/example/circletest/CircleTestApplication.java
@@ -1,12 +1,17 @@
 package com.example.circletest;
 
+import static java.lang.Thread.sleep;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CircleTestApplication {
-
-	public static void main(String[] args){
-		SpringApplication.run(CircleTestApplication.class, args);
-	}
+  public static void main(String[] args) {
+    try {
+      sleep(1);
+    } catch (InterruptedException ignore) {
+    }
+    SpringApplication.run(CircleTestApplication.class, args);
+  }
 }
